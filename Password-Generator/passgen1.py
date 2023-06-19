3ef66984dee20d47fbd5aedde7081266a9ae83d1
TurtlesXD <116833814+TurtlesXD@users.noreply.github.com>
2023-06-19 20:08:16 +0100

    Update passgen1.py
    Automatically copys password to clipboard. Also added run code.

--- Password-Generator/passgen1.py
+++ Password-Generator/passgen1.py
@@ -1,5 +1,6 @@
 import secrets
 import string
+import pyperclip
 
 letters = string.ascii_letters
 digits = string.digits
@@ -7,10 +8,14 @@ special_chars = string.punctuation
 alphabet= letters+digits+special_chars
 password=' '
 
-
 def generatepass(length):
     global password
     for i in range(int(length)):
         password += ''.join(secrets.choice(alphabet))
-    print(password)   
-generatepass(12)     
+    print(password) 
+    pyperclip.copy(password)  
+
+if __name__ == '__main__':
+    generatepass(12)  
+       
+    
