6ce40784efe7e935c07beb5e5d799f234d74ab62
TurtlesXD <116833814+TurtlesXD@users.noreply.github.com>
2023-06-19 20:42:26 +0100

    Create MultiPassGen.py
    A password generator that can make multiple passwords.

---
+++ Password-Generator/MultiPassGen.py
@@ -0,0 +1,29 @@
+import secrets
+import string
+import pyperclip
+
+lenamt=input('>Enter Password Length: ')
+passamt=input('>Enter Password Amount: ')
+letters = string.ascii_letters
+digits = string.digits
+special_chars = string.punctuation
+alphabet= letters+digits+special_chars
+password=' '
+multipassword=[ ]
+
+def generatepass(length,pamt):
+    global password
+    global multipassword
+    for i in range(int(pamt)):
+     print(password)
+     pyperclip.copy(password)
+     password=''
+     for i in range(int(length)):
+         password += ''.join(secrets.choice(alphabet))
+    print(password) 
+     
+
+if __name__ == '__main__':
+    generatepass(lenamt,passamt)  
+       
+    
